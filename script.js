90eb22564306fcc5538127393db0ec1398fb09f2
Juliano <jheylprod08@gmail.com>
2022-06-26 20:46:17 -0300

    AtualizaListadeMensagens

--- script.js
+++ script.js
@@ -4,55 +4,54 @@ let receiver;
 let menssagem;
 let texto;
 let todasmensagens = [];
-let meuintervalo
+let usuario = [];
+let meuintervalo;
+perguntanome();
 
+function pegamenssagens(){
 const promise = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages");
 promise.then(recebedados);
+}
 
-new function perguntanome() {
-    let numvef;
+function perguntanome() {
     nome = String(prompt("qual seu nome lindo?"));
-    if (!nome) {
-        numvef = 0;
-        console.log("ainda não");
-    } else {
-        numve = 2;
-        console.log("ufa");
+    usuario = {
+        name: nome
     }
-    while (numvef < 1) {
-        nome = String(prompt("qual seu nome lindo?"));
-        if (nome) {
-            numvef = 1;
-            console.log("ufa");
-        }
-        
-    }
-    entrou(); 
+    const promisse2 = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants", usuario);
+    promisse2.then(pegamenssagens());
+
+    promisse2.catch(entrarErro())
+}
+function entrarErro(error) {
+    window.alert("esta nome ja esta sendo usado ou é invalido, tente novamente");
+}
+function atulizaLogin() {
+    
 }
 function entrou() {
     let notification = `<li>
-                    <div>
-                        <p class="message">(09:21:45) ${nome} entrou na sala</p>
+                    <div class="status">
+                        <p class="text">(09:21:45) <strong>${nome}</strong> entrou na sala...</p>
                     </div>
                 </li>`;
     document.querySelector(".listachat").innerHTML += notification;
-    meuintervalo = setInterval(crialista2(), 3000);
 }
 
 function teste2() {
+
     receiver = "todos";
     menssagem = document.querySelector(".chatinput");
     texto = menssagem.value;
     let notification = `<li>
                     <div>
-                        <p class="message">(09:21:45) ${nome} para <strong>${receiver}</strong>: ${texto} </p>
+                        <p class="message">(09:21:45) <strong>${nome}</strong> para <strong>${receiver}</strong>: ${texto} </p>
                     </div>
                 </li>`;
     document.querySelector(".listachat").innerHTML += notification;
 }
 
 function recebedados(dados) {
-    console.log(dados.data);
     todasmensagens = dados.data;
     crialista2();
 }
@@ -60,23 +59,18 @@ function recebedados(dados) {
 function teste() {
     let menssagem = document.querySelector(".chatinput");
     let texto = menssagem.value;
-    console.log(texto);
 }
 
 function crialista2() {
-    console.log("to atualizando")
     const listavazia = document.querySelector(".listachat");
-    let mostra;
-    mostra = document.querySelector('message');
+    listavazia.innerHTML = "";
     for (let i = 0; i < todasmensagens.length; i++){
         listavazia.innerHTML += 
-            `<li>
-                    <div>
-                        <p class="message">(${todasmensagens[i].time}) ${todasmensagens[i].from} para <strong>${todasmensagens[i].to}</strong>: ${todasmensagens[i].text} </p>
-                    </div>
-                </li>`;
-        mostra = document.querySelector('message');
+            `<li class="elementolista"> 
+                <div class="${todasmensagens[i].type}">
+                    <p class="text ${todasmensagens[i].type}">(${todasmensagens[i].time})  <strong> ${todasmensagens[i].from}</strong> para <strong>${todasmensagens[i].to}</strong>: ${todasmensagens[i].text}</p>
+                </div>
+            </li>`;
     }
-    
+    setTimeout(pegamenssagens, 3000);
 }
-    
