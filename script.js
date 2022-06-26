5d6fcf866e0c5131c5e0b319fe5098539148a37c
Juliano <jheylprod08@gmail.com>
2022-06-26 15:49:04 -0300

    SoAceitaNomesValidos

--- script.js
+++ script.js
@@ -1,14 +1,43 @@
 let nickname;
-let nome;
+let nome = "";
 let receiver;
 let menssagem;
 let texto;
 let todasmensagens = [];
-
-nome = window.prompt("qual seu nome lindo?");
+let meuintervalo
 
 const promise = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages");
 promise.then(recebedados);
+
+new function perguntanome() {
+    let numvef;
+    nome = String(prompt("qual seu nome lindo?"));
+    if (!nome) {
+        numvef = 0;
+        console.log("ainda não");
+    } else {
+        numve = 2;
+        console.log("ufa");
+    }
+    while (numvef < 1) {
+        nome = String(prompt("qual seu nome lindo?"));
+        if (nome) {
+            numvef = 1;
+            console.log("ufa");
+        }
+        
+    }
+    entrou(); 
+}
+function entrou() {
+    let notification = `<li>
+                    <div>
+                        <p class="message">(09:21:45) ${nome} entrou na sala</p>
+                    </div>
+                </li>`;
+    document.querySelector(".listachat").innerHTML += notification;
+    meuintervalo = setInterval(crialista2(), 3000);
+}
 
 function teste2() {
     receiver = "todos";
@@ -17,15 +46,6 @@ function teste2() {
     let notification = `<li>
                     <div>
                         <p class="message">(09:21:45) ${nome} para <strong>${receiver}</strong>: ${texto} </p>
-                    </div>
-                </li>`;
-    document.querySelector(".listachat").innerHTML += notification;
-}
-
-function entrou() {
-    let notification = `<li>
-                    <div>kk
-                        <p class="message">(09:21:45) ${nome} entrou na sala</p>
                     </div>
                 </li>`;
     document.querySelector(".listachat").innerHTML += notification;
@@ -44,7 +64,10 @@ function teste() {
 }
 
 function crialista2() {
+    console.log("to atualizando")
     const listavazia = document.querySelector(".listachat");
+    let mostra;
+    mostra = document.querySelector('message');
     for (let i = 0; i < todasmensagens.length; i++){
         listavazia.innerHTML += 
             `<li>
@@ -52,8 +75,8 @@ function crialista2() {
                         <p class="message">(${todasmensagens[i].time}) ${todasmensagens[i].from} para <strong>${todasmensagens[i].to}</strong>: ${todasmensagens[i].text} </p>
                     </div>
                 </li>`;
+        mostra = document.querySelector('message');
     }
+    
 }
-
-crialista2();
     
