eff06820c3bbe4aa8e7f961ef08256967f8103a5
Juliano <jheylprod08@gmail.com>
2022-06-26 21:59:42 -0300

    ProcuraUltimaMensssagem

--- script.js
+++ script.js
@@ -8,26 +8,29 @@ let usuario = [];
 let meuintervalo;
 perguntanome();
 
-function pegamenssagens(){
-const promise = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages");
-promise.then(recebedados);
+function pegamenssagens() { 
+    const promise = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages");
+    promise.then(recebedados);
 }
-
+function atulizaLogin() {
+    console.log("atualizei o login");
+    const promisse2 = axios.post("https://mock-api.driven.com.br/api/v6/uol/status", usuario);
+}
+function fazlogin() {
+    const promisse2 = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants", usuario);
+    promisse2.then(pegamenssagens());
+    promisse2.catch(entrarErro());
+    const intervalo = setInterval(atulizaLogin, 4999);
+}
 function perguntanome() {
     nome = String(prompt("qual seu nome lindo?"));
     usuario = {
         name: nome
     }
-    const promisse2 = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants", usuario);
-    promisse2.then(pegamenssagens());
-
-    promisse2.catch(entrarErro())
+    fazlogin();
 }
 function entrarErro(error) {
     window.alert("esta nome ja esta sendo usado ou é invalido, tente novamente");
-}
-function atulizaLogin() {
-    
 }
 function entrou() {
     let notification = `<li>
@@ -37,28 +40,23 @@ function entrou() {
                 </li>`;
     document.querySelector(".listachat").innerHTML += notification;
 }
-
-function teste2() {
-
-    receiver = "todos";
-    menssagem = document.querySelector(".chatinput");
-    texto = menssagem.value;
-    let notification = `<li>
-                    <div>
-                        <p class="message">(09:21:45) <strong>${nome}</strong> para <strong>${receiver}</strong>: ${texto} </p>
-                    </div>
-                </li>`;
-    document.querySelector(".listachat").innerHTML += notification;
+function teste() {
+    local = document.querySelector(".chatinput");
+    texto = local.value;
+    if (texto != null) {
+        console.log(texto);
+        menssagem = {
+            from: nome,
+            to: "Todos",
+            text: texto,
+            type: "message"
+        }
+    }
+    const promisse3 = axios.post("https://mock-api.driven.com.br/api/v6/uol/messages", menssagem);
 }
-
 function recebedados(dados) {
     todasmensagens = dados.data;
     crialista2();
-}
-
-function teste() {
-    let menssagem = document.querySelector(".chatinput");
-    let texto = menssagem.value;
 }
 
 function crialista2() {
@@ -72,5 +70,8 @@ function crialista2() {
                 </div>
             </li>`;
     }
+    const elementopai = document.querySelector('.listachat');
+    const elementoQueQueroQueApareca = elementopai.lastElementChild;
+    elementoQueQueroQueApareca.scrollIntoView();
     setTimeout(pegamenssagens, 3000);
 }
