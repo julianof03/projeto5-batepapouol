5b4b00334688a5ff9cefb37b3c18d57b40926f8c
Juliano <jheylprod08@gmail.com>
2022-06-26 23:30:28 -0300

    versãoFinal

--- script.js
+++ script.js
@@ -4,55 +4,53 @@ let receiver;
 let menssagem;
 let texto;
 let todasmensagens = [];
-let usuario = [];
+let usuario;
 let meuintervalo;
+let promise;
+let promise2;
+let promise3;
 perguntanome();
 
 function pegamenssagens() { 
     const promise = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages");
     promise.then(recebedados);
 }
-function atulizaLogin() {
-    console.log("atualizei o login");
-    const promisse2 = axios.post("https://mock-api.driven.com.br/api/v6/uol/status", usuario);
-}
-function fazlogin() {
-    const promisse2 = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants", usuario);
-    promisse2.then(pegamenssagens());
-    promisse2.catch(entrarErro());
-    const intervalo = setInterval(atulizaLogin, 4999);
-}
 function perguntanome() {
-    nome = String(prompt("qual seu nome lindo?"));
-    usuario = {
+    nome = prompt("qual seu nome lindo?");
+        promise2 = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants",
+    {
         name: nome
     }
-    fazlogin();
+    );
+    promise2.then(pegamenssagens);
+    promise2.catch(entrarErro);
+    setInterval(atulizaLogin, 5000);
+}
+function atulizaLogin() {
+    console.log("atualizei o login");
+    const promisse2 = axios.post("https://mock-api.driven.com.br/api/v6/uol/status",
+        {
+            name: nome
+        });
 }
 function entrarErro(error) {
-    window.alert("esta nome ja esta sendo usado ou é invalido, tente novamente");
-}
-function entrou() {
-    let notification = `<li>
-                    <div class="status">
-                        <p class="text">(09:21:45) <strong>${nome}</strong> entrou na sala...</p>
-                    </div>
-                </li>`;
-    document.querySelector(".listachat").innerHTML += notification;
+    console.log(error);
+    location.reload();
 }
 function teste() {
     local = document.querySelector(".chatinput");
-    texto = local.value;
-    if (texto != null) {
-        console.log(texto);
-        menssagem = {
-            from: nome,
-            to: "Todos",
-            text: texto,
-            type: "message"
-        }
+    texto = local.value;  
+    console.log(texto);
+    menssagem = {
+        from: nome,
+        to: "Todos",
+        text: texto,
+        type: "message"
     }
-    const promisse3 = axios.post("https://mock-api.driven.com.br/api/v6/uol/messages", menssagem);
+    const promise = axios.post("https://mock-api.driven.com.br/api/v6/uol/messages", menssagem);
+    promise.catch(function () {
+        location.reload();
+    })
 }
 function recebedados(dados) {
     todasmensagens = dados.data;
